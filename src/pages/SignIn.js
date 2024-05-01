844eacf7e8b3d34f07180ccab29bcd879d3dffac
Sroopesh1122 <shivuroopesh6362@gmail.com>
2024-05-01 09:31:40 +0530

    upadates

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -17,7 +17,7 @@ const SignIn = () => {
   };
 
   const validationSchema = Yup.object().shape({
-    username: Yup.string().required("username is required"),
+    username: Yup.string().required("username is  required!"),
     password: Yup.string().required("password is required"),
   });
 
